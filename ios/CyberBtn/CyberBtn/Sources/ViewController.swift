541f4c7de73a2f02a39d1f4be1c57949ec577b6f
Kyle Ilantzis <ilantzis.kyle@gmail.com>
2021-01-30 11:32:34 -0500

    ios text glitch shadow

--- ios/CyberBtn/CyberBtn/Sources/ViewController.swift
+++ ios/CyberBtn/CyberBtn/Sources/ViewController.swift
@@ -10,15 +10,24 @@ import UIKit
 
 let kCyYellow10 = UIColor(red: 255.0/255.0, green: 247/255.0, blue: 0, alpha: 1)
 let kCyYellow20 = UIColor(red: 245/255.0, green: 245/255.0, blue: 61.0/255.0, alpha: 1)
+let kCyYellow30 = UIColor(red: 255/255.0, green: 237/255.0, blue: 0, alpha: 1)
+let kCyYellow40 = UIColor(red: 230/255.0, green: 222/255.0, blue: 0, alpha: 1)
 let kCyRed = UIColor(red: 236.0/255.0, green: 19.0/255.0, blue: 19.0/255.0, alpha: 1)
 let kCyBlue = UIColor(red: 13.0/255.0, green: 242.0/255.0, blue: 242.0/255.0, alpha: 1)
 
 class ViewController: UIViewController {
+
+    let yellowGradientBackgroud = CAGradientLayer()
+
     override func loadView() {
         let view = UIView()
-        view.backgroundColor = kCyYellow10
+        self.yellowGradientBackgroud.colors = [kCyYellow30.cgColor, kCyYellow40.cgColor]
+        self.yellowGradientBackgroud.locations = [NSNumber(value: 0.75), NSNumber(value: 0.75)]
+        self.yellowGradientBackgroud.startPoint = CGPoint(x: 0.0, y: 0.5)
+        self.yellowGradientBackgroud.endPoint = CGPoint(x: 1.0, y: 0.5)
+        view.layer.addSublayer(self.yellowGradientBackgroud)
 
-        let button = CyberButton()
+        let button = CyberButton.create()
         button.setTitle("Clipped_", for: .normal)
         button.setTag("R25")
 
@@ -35,6 +44,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.yellowGradientBackgroud.frame = self.view.frame
+    }
 }
 
 let kMinWidth: CGFloat = 250.0
@@ -45,18 +59,27 @@ let kTagFontSize: CGFloat = 9.0
 let kTagInset: CGFloat = 2.0
 let kTagShadowOffsetX: CGFloat = 2.0
 let kTagBottomOffset: CGFloat = (kTagFontSize + kTagInset)/2
+let kGlitchTextShadowOffset: CGFloat = 2.0
 
 final class CyberButton: UIButton {
     private let maskLayer = CAShapeLayer()
 
     private let tagLabel = UILabel()
     private let tagView = UIView()
+    private var glitchButton: CyberButton?
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    init() {
+    static func create() -> CyberButton {
+        let button = CyberButton()
+        let glitch = CyberButton()
+        button.addGlitch(glitch)
+        return button
+    }
+
+    private init() {
         super.init(frame: .zero)
         self.titleLabel?.textColor = .white
         self.titleLabel?.font = UIFont(name: "BlenderPro-Heavy", size: 26.0)
@@ -107,16 +130,56 @@ final class CyberButton: UIButton {
             self.tagView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: kTagBottomOffset),
             self.tagView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -kInsetHorizontal/6)
         ])
+
+        self.tagView.isHidden = true
     }
 
     override func setTitle(_ title: String?, for state: UIControl.State) {
         let attrTitle = NSAttributedString(string: (title ?? "").uppercased(), attributes: [NSAttributedString.Key.kern: 2.0])
         super.setAttributedTitle(attrTitle, for: state)
+
+        let glitchYellowTextShadow = NSShadow()
+        glitchYellowTextShadow.shadowColor = kCyYellow10
+        glitchYellowTextShadow.shadowBlurRadius = 0
+        glitchYellowTextShadow.shadowOffset = CGSize(width: -kGlitchTextShadowOffset, height: -kGlitchTextShadowOffset)
+
+        let glitchAttrTitle = NSAttributedString(string: (title ?? "").uppercased(), attributes: [
+            NSAttributedString.Key.kern: 2.0,
+            NSAttributedString.Key.shadow: glitchYellowTextShadow
+        ])
+
+        self.glitchButton?.setAttributedTitle(glitchAttrTitle, for: state)
     }
 
     func setTag(_ tag: String?) {
         let attrTag = NSAttributedString(string: (tag ?? "").uppercased(), attributes: [NSAttributedString.Key.kern: 1.0])
         self.tagLabel.attributedText = attrTag
+        self.tagView.isHidden = tag == nil && tag == ""
+    }
+
+    private func addGlitch(_ glitchButton: CyberButton) {
+        self.glitchButton = glitchButton
+        glitchButton.isUserInteractionEnabled = false
+
+        glitchButton.titleLabel?.layer.shadowColor = kCyBlue.cgColor
+        glitchButton.titleLabel?.layer.shadowOpacity = 1
+        glitchButton.titleLabel?.layer.shadowRadius = 0
+        glitchButton.titleLabel?.layer.shadowOffset = CGSize(width: kGlitchTextShadowOffset, height: kGlitchTextShadowOffset)
+
+        let glitchBlueTextShadow = NSShadow()
+        glitchBlueTextShadow.shadowColor = kCyBlue
+        glitchBlueTextShadow.shadowBlurRadius = 0
+        glitchBlueTextShadow.shadowOffset = CGSize(width: kGlitchTextShadowOffset, height: kGlitchTextShadowOffset)
+
+        self.insertSubview(glitchButton, belowSubview: self.tagView)
+
+        NSLayoutConstraint.activate([
+            self.widthAnchor.constraint(greaterThanOrEqualToConstant: kMinWidth),
+            glitchButton.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            glitchButton.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            glitchButton.topAnchor.constraint(equalTo: self.topAnchor),
+            glitchButton.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+        ])
     }
 
     override var bounds: CGRect {
