f8341578b0ce7264dd44a0f50039fcd541338b68
Kyle Ilantzis <ilantzis.kyle@gmail.com>
2021-01-30 00:07:38 -0500

    ios btn mask

--- ios/CyberBtn/CyberBtn/Sources/ViewController.swift
+++ ios/CyberBtn/CyberBtn/Sources/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Kyle on 2021-01-29.
 //
 
+import CoreGraphics
 import UIKit
 
 let kCyYellow = UIColor(red: 255.0/255.0, green: 247/255.0, blue: 0, alpha: 1)
@@ -31,14 +32,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        for family in UIFont.familyNames.sorted() {
-            let names = UIFont.fontNames(forFamilyName: family)
-            print("Family: \(family) Font names: \(names)")
-        }
     }
 }
 
+let kShadowOfferX: CGFloat = 3.0
+
 final class CyberButton: UIButton {
+    private let maskLayer = CAShapeLayer()
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -56,11 +57,30 @@ final class CyberButton: UIButton {
         self.layer.shadowColor = kCyBlue.cgColor
         self.layer.shadowOpacity = 1
         self.layer.shadowRadius = 0
-        self.layer.shadowOffset = CGSize(width: 3, height: 0)
+        self.layer.shadowOffset = CGSize(width: kShadowOfferX, height: 0)
+
+        self.layer.mask = self.maskLayer
     }
 
     override func setTitle(_ title: String?, for state: UIControl.State) {
         let attrTitle = NSAttributedString(string: (title ?? "").uppercased(), attributes: [NSAttributedString.Key.kern: 2.0])
         super.setAttributedTitle(attrTitle, for: state)
+    }
+
+    override var bounds: CGRect {
+        get { super.bounds }
+        set {
+            super.bounds = newValue
+            self.maskLayer.frame = newValue
+
+            let path = CGMutablePath()
+            path.move(to: .zero)
+            path.addLine(to: CGPoint(x: newValue.width + kShadowOfferX, y: 0))
+            path.addLine(to: CGPoint(x: newValue.width + kShadowOfferX, y: newValue.height))
+            path.addLine(to: CGPoint(x: 0, y: newValue.height))
+            path.addLine(to: .zero)
+
+            self.maskLayer.path = path.copy()
+        }
     }
 }
