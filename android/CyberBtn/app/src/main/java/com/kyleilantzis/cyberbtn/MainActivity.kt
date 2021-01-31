bf67e99efb012bde02d70447dafb3663e413f6f4
Kyle Ilantzis <ilantzis.kyle@gmail.com>
2021-01-31 00:49:48 -0500

    android cyber red

--- android/CyberBtn/app/src/main/java/com/kyleilantzis/cyberbtn/MainActivity.kt
+++ android/CyberBtn/app/src/main/java/com/kyleilantzis/cyberbtn/MainActivity.kt
@@ -2,10 +2,7 @@ package com.kyleilantzis.cyberbtn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.Window
-import android.view.WindowInsets
-import android.view.WindowManager
+import android.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
