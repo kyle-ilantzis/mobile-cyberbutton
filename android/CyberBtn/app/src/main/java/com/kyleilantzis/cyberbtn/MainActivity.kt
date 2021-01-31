b4084aeeadfd5a2b9939e636302635c907f524d5
Kyle Ilantzis <ilantzis.kyle@gmail.com>
2021-01-30 22:31:26 -0500

    android fullscreen

--- android/CyberBtn/app/src/main/java/com/kyleilantzis/cyberbtn/MainActivity.kt
+++ android/CyberBtn/app/src/main/java/com/kyleilantzis/cyberbtn/MainActivity.kt
@@ -2,10 +2,17 @@ package com.kyleilantzis.cyberbtn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
     }
 }
