e51663df4ff83bfbb08eed19411a2bdd614acec1
Kyle Ilantzis <ilantzis.kyle@gmail.com>
2021-01-31 14:10:16 -0500

    android clipped

--- android/CyberBtn/app/src/main/java/com/kyleilantzis/cyberbtn/MainActivity.kt
+++ android/CyberBtn/app/src/main/java/com/kyleilantzis/cyberbtn/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.kyleilantzis.cyberbtn
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +16,40 @@ class MainActivity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+    }
+}
+
+
+
+class CyberButton(ctx: Context, attrSet: AttributeSet): androidx.appcompat.widget.AppCompatButton(ctx, attrSet) {
+
+    val kMaskPathInset = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10.0F, resources.displayMetrics)
+    val kInsetHorizontal = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40.0F, resources.displayMetrics)
+
+    var clipPath = Path()
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        clipPath = createMaskPath(measuredWidth, measuredHeight)
+    }
+
+    override fun draw(canvas: Canvas?) {
+        canvas?.clipPath(clipPath)
+        super.draw(canvas)
+    }
+
+    private fun createMaskPath(mw: Int, mh: Int): Path {
+        val width = mw.toFloat() + kMaskPathInset
+        val height = mh.toFloat() + kMaskPathInset
+
+        val path = Path()
+        path.moveTo(-kMaskPathInset, -kMaskPathInset)
+        path.lineTo(width, -kMaskPathInset)
+        path.lineTo(width, height)
+        path.lineTo(kMaskPathInset + kInsetHorizontal*2/3, height)
+        path.lineTo(-kMaskPathInset, height - kInsetHorizontal*3/3)
+        path.lineTo(-kMaskPathInset, -kMaskPathInset)
+
+        return path
     }
 }
